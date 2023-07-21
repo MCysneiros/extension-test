c21310225dc84eff66bec72960bd33212a5a29e0
Jonatã Oliveira <jonata.96ol@gmail.com>
2023-07-21 12:50:25 -0300

    test receive message

--- background.js
+++ background.js
@@ -1,7 +1,10 @@
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-	if (message === 'hello world') {
-		chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-			chrome.tabs.sendMessage(tabs[0].id, 'hello world')
+	if (message.type === 'openWindow') {
+		chrome.windows.create({
+			url: 'window.html',
+			type: 'popup',
+			width: 300,
+			height: 200,
 		})
 	}
 })
--- inject.js
+++ inject.js
@@ -1,23 +1,13 @@
 function sendHello() {
 	return new Promise((resolve) => {
-		chrome.windows.create(
-			{
-				url: 'window.html',
-				type: 'popup',
-				width: 300,
-				height: 200,
-			},
-			function (window) {
-				const windowId = window.id
-				chrome.runtime.onMessage.addListener(function listener(message) {
-					if (message === 'hello world') {
-						chrome.runtime.onMessage.removeListener(listener)
-						chrome.windows.remove(windowId)
-						resolve(message)
-					}
-				})
+		chrome.runtime.sendMessage({ type: 'openWindow' })
+
+		chrome.runtime.onMessage.addListener(function listener(message) {
+			if (message === 'hello world') {
+				chrome.runtime.onMessage.removeListener(listener)
+				resolve(message)
 			}
-		)
+		})
 	})
 }
 
